76fe126699d1bdff0ae1cbc1f2587f6267b919b0
Rheyno Apria Pratama <rheynoapria11@gmail.com>
2018-09-05 21:14:43 +0700

    add balikKata.js

---
+++ balikKata.js
@@ -0,0 +1,7 @@
+function balikKata(kata){
+  return kata.split('').reverse().join('')
+}
+
+console.log(balikKata("Jakarta"));
+console.log(balikKata("Love_Programming"));
+console.log(balikKata("JavaScript"));
